9b69b481f30efd60f63771b75b25f1e99254c4ac
bilal-gujjar <bilaltahir515@gmail.com>
2022-02-15 20:24:30 +0500

    Add Git with hosting

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <h1>Welcome to Blessed Builders</h1>
+      <h1>Welcome to Blessed-Builders</h1>
       <h3>Our Site is under Development</h3>
       </header>
     </div>
